d7b4430c2aea25373d63a1bf7701592e1c843f30
labeveryday <29664932+labeveryday@users.noreply.github.com>
2021-08-23 10:19:52 -0500

    Added day 2 quiz

---
+++ quiz2.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+"""
+Date: 8-23-2021
+Twitter Link: https://twitter.com/labeveryday/status/1429469481559117826?s=20
+Results:
+
+Python quiz question for the day?
+
+bills = ["house", "car", "phone", "onlyfans"]
+
+for bill in range(len(bills)):
+    print(bill)
+
+If we execute this code, which bill will print?
+
+#LabEverydayDailyPythonQuiz
+
+A.All items
+B. None
+C. onlyfans
+D. TypeError
+"""
+bills = ["house", "car", "phone", "onlyfans"]
+
+for bill in range(len(bills)):
+    print(bill)
