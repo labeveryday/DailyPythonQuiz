90be6f21f2890002527225dd011cc3ce16331197
labeveryday <29664932+labeveryday@users.noreply.github.com>
2021-08-26 22:54:07 -0500

    Added quiz5

---
+++ quiz5.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+"""
+Date: 8-26-2021
+Twitter Link: https://twitter.com/labeveryday/status/1430501257580584972?s=20
+Documentation: https://docs.python.org/3/reference/lexical_analysis.html#keywords
+-----------------------------------------------------------------------------
+
+Python quiz question for the day?
+
+Which of these are a valid variable name in Python?
+
+#LabEverydayDailyPythonQuiz #Day5
+
+A. True
+B. 1_name
+C. my-name
+D. _
+"""
+
+# Assign string variable
+True = "Test"
+1_name = "Test"
+my-name = "Test"
+_ = "Test"
