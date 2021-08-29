72b02eeea8e107f17531d51819a3d07643a7b532
Du'An Lightfoot <duanl@labeveryday.com>
2021-08-29 18:09:15 -0500

    Added quiz 7

--- quiz7.py
+++ quiz7.py
@@ -20,6 +20,7 @@ If we execute this code, what is the result?
 def add_num1():
     print(3 + 4)
 
+
 # Example 2: How to call a function
 add_num1()
 
@@ -31,18 +32,19 @@ def add_num2():
 x = add_num2()
 print(f"X is {x}")
 
+
 def add_num3():
     return 3 + 4
 
 y = add_num3()
-print(f"X is {y}")
+print(f"Y is {y}")
 
 
 # Example 4: How to pass arguments into a function?
 def add_num4(x, y):
     return x + y
 
-print(add_num4(10, 22))
+print(add_num4(50, 25))
 
 
 # Example 5: Understanding standing positional arguments.
@@ -50,35 +52,28 @@ def print_name(first_name, last_name):
     print(f"My first name is {first_name}")
     print(f"My last name is {last_name}")
 
-print_name("Du'An", "Lightfoot")
 print_name("Lightfoot", "Du'An")
+print_name("Lightfoot", "Du'An")
+print_name(first_name="Du'An", Lightfoot="Lightfoot")
 
 
-# Example 6: Using Using Keyword Arguments
+# Example 6: Using Keyword Arguments
 def print_name2(first_name, last_name):
     print(f"My first name is {first_name}")
     print(f"My last name is {last_name}")
 
-print_name("Lightfoot", first_name="Du'An")
-print_name(first_name="Du'An", "Lightfoot")
-print_name(first_name="Du'An", Lightfoot="Lightfoot")
-
+print_name2("Du'An", fist_name="Lightfoot")
+print_name2(last_name="Lightfoot", first_name="Du'An")
 
 # Example 7: Assigning default values
-def print_name2(first_name, last_name="Du'An"):
+def print_name2(first_name, last_name="Lightfoot"):
     print(f"My first name is {first_name}")
     print(f"My last name is {last_name}")
 
-print_name2("Lightfoot")
-print_name("Lightfoot", first_name="Bob")
+print_name2(first_name="Du'An", last_name="Lightfoot")
 
+# Quiz Question - This will fail!
+def add_num(x=4, y):
+    return x + y
 
-
-
-
-
-# Quiz Question
-# def add_num(x=4, y):
-#     return x + y
-# 
-# print(add_num(10))
+print(add_num(10))
